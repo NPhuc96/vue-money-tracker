089111edd3d2249b7e49c16a1184b02ccde7e567
phuc <nguyenphuc.vna@gmail.com>
2021-09-30 11:05:54 +0700

    added title to router

--- src/router.js
+++ src/router.js
@@ -15,13 +15,20 @@ const router = createRouter({
       path: "/",
       component: Home,
       name: "home",
+      meta: { title: "Home" },
       children: [
         {
           path: "add/transaction",
           component: AddTransaction,
           name: "addTransaction",
+          meta: { title: "Add Expense" },
         },
-        { path: "add/group", component: AddGroup, name: "addGroup" },
+        {
+          path: "add/group",
+          component: AddGroup,
+          name: "addGroup",
+          meta: { title: "Add Group" },
+        },
       ],
     },
     {
@@ -29,22 +36,35 @@ const router = createRouter({
       component: Authentication,
       name: "auth",
       children: [
-        { path: "login", component: Login, name: "login" },
-        { path: "signup", component: Signup, name: "signup" },
+        {
+          path: "login",
+          component: Login,
+          name: "login",
+          meta: { title: "Login" },
+        },
+        {
+          path: "signup",
+          component: Signup,
+          name: "signup",
+          meta: { title: "Sign Up" },
+        },
       ],
     },
     {
       path: "/auth/confirm",
       component: Confirmation,
       name: "confirmation",
+      meta: { title: "Confirmation" },
     },
   ],
 });
 router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
   if (!to.path.includes("auth") && (!isTimeValid() || !isAuthenticated())) {
     next({ name: "login" });
   } else next();
 });
+
 
 const isAuthenticated = () => {
   return localStorage.getItem("token");
